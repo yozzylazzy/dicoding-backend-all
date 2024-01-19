b54faa9bc74de74e4a0cbf5193499a640fc43eac
Yozzy Lazzy <kristian_yosef@yahoo.com>
2024-01-19 19:48:43 +0700

    init commit projek akhir backend pemula

---
+++ backend-pemula/proyek-akhir/src/handler.js
@@ -0,0 +1,34 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name, year, author, summary, publisher, pageCount, readPage, reading,
+  } = request.payload;
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  if (name !== undefined && name !== '' && readPage <= pageCount) {
+    books.push(newBook);
+  }
+};
+
+module.exports = {
+  addBookHandler,
+};
